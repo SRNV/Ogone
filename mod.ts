d6f704fcd0fd35e94bba374c17acdbf940e7a14c
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-27 13:41:56 +0530

    fix: compilation error

--- mod.ts
+++ mod.ts
@@ -114,6 +114,7 @@ async function run(opts: OgoneOptions): Promise<void> {
       .then(async () => {
         //start compilation of o3 files
         const b = await EnvServer.getBuild();
+	       /**
         const application = `${opts.build}/index.html`;
         Deno.writeTextFileSync(application, b);
         console.warn(
@@ -126,6 +127,7 @@ async function run(opts: OgoneOptions): Promise<void> {
           server.close();
           Deno.exit();
         }
+        **/
       });
   } else {
     //start compilation of o3 files
