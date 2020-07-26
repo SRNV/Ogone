d9adb3525c625400d3082b3049f7a06e044d7464
SRNV <alula.rudy@gmail.com>
2020-07-26 20:28:35 +0200

    wip

--- cli/o3.ts
+++ cli/o3.ts
@@ -1,28 +1,49 @@
 // @ts-nocheck
-import { parseFlags, OptionType, InputCLI, Confirm, Select } from './deps.ts';
+import { parseFlags, OptionType, InputCLI, Confirm, Select, existsSync } from './deps.ts';
 
 class OgoneCLI {
-  private templateSTR: string = `
-    {{ templateImports }}
-    {{ templateProto }}
-    {{ templateStore }}
-    {{ templateStyle }}
-  `;
-  private templateProto: string = `
-    <proto{{ componentType }}>
-      {{ def }}
-      {{ declarations }}
-      {{ beforeEach }}
-      {{ default }}
-    </proto>`;
-  private templateStyle: string = `
-    <style{{ styleLang }}>
+  private static templateSTR: string = `
+{{ templateImports }}
+{{ templateStore }}
+{{ templateProto }}
+{{ elements }}
+{{ templateStyle }}`;
+  private static templateProto: string = `
+<proto{{ componentType }}>
+  {{ def }}
+  {{ declarations }}
+  {{ beforeEach }}
+  {{ defaultSTR }}
+</proto>`;
+  private static templateStyle: string = `
+<style{{ styleLang }}>
 
-    </style>`;
-  private templateStore: string = `
-    <store-component {{ storeNamespace }} />
-  `;
-  private templateImports: string = `// {{ path }}/{{ componentName }}.o3`;
+</style>`;
+  private static templateStore: string = `<store-component {{ storeNamespace }} />`;
+  private static templateImports: string = `// {{ path }}/{{ componentName }}.o3`;
+  private static template(tmpl: string, data: any): string {
+    let result = tmpl;
+    const fn = new Function(
+      "__value",
+      ...Object.keys(data),
+      `try { return eval('('+ __value + ')'); } catch(err) { throw err; }`,
+    );
+    const values = Object.values(data);
+    while (
+      result.indexOf("{{") > -1 && result.indexOf("}}") > -1
+    ) {
+      if (result.indexOf("{{") > result.indexOf("}}")) {
+        result = result.replace("}}", "} }");
+      }
+      const start = result.indexOf("{{");
+      const end = result.indexOf("}}") + 2;
+      const substrContent = result.substring(start + 2, end - 2).trim();
+      const partStart = result.substring(0, start);
+      const partEnd = result.substring(end);
+      result = partStart + fn(substrContent, ...values) + partEnd;
+    }
+    return result;
+  }
   public static async init() {
     parseFlags(Deno.args, {
       flags: [
@@ -45,44 +66,83 @@ class OgoneCLI {
   private static async promptCreation(value: string) {
     switch (true) {
       case value && ['comp', 'component', 'c'].includes(value):
-        /*
-        const cssLang: string = await Select.prompt({
-          messsage: 'CSS pre-processor ?',
+        let pathToFolder: string = await InputCLI.prompt(`path to the component's folder`);
+        while(!existsSync(pathToFolder)) {
+          console.warn('Invalid path.')
+          pathToFolder = await InputCLI.prompt(`path to the component's folder`);
+        }
+        if (pathToFolder.endsWith('/')) {
+          pathToFolder = pathToFolder.slice(0, -1);
+        }
+        let componentName: string = await InputCLI.prompt(`component's name ?`);
+        while(existsSync(`${pathToFolder}/${componentName}.o3`)) {
+          console.warn('component already exists.', `${pathToFolder}/${componentName}.o3`)
+          componentName = await InputCLI.prompt(`component's name ?`);
+        }
+        if (componentName.endsWith('.o3')) {
+          componentName = componentName.slice(0, -3);
+        }
+        const fullPath = `${pathToFolder}/${componentName}.o3`;
+        let usingDef: boolean = false,
+        usingDefault: boolean = false,
+        usingDeclare: boolean = false,
+        typeofComponent: string = 'component',
+        usingBeforeEach: boolean = false;
+        typeofComponent = await Select.prompt({
+          message: 'type of the component ?',
           options: [
-            { name: 'Sass', value: 'sass' },
-            { name: 'Denolus', value: 'denolus' },
+            { name: 'async', value: 'async' },
+            { name: 'store', value: 'store' },
+            { name: 'router', value: 'router' },
+            { name: 'component', value: 'component' },
+            { name: 'controller', value: 'controller' },
           ]
         });
-        */
-        const pathToFolder: string = await InputCLI.prompt(`path to the component's folder`);
-        const componentName: string = await InputCLI.prompt(`component's name ?`);
-        const hasProto: boolean = await Confirm.prompt(`insert a proto element ?`);
-        let usingDef: boolean = false,
-          usingDefault: boolean = false,
-          usingDeclare: boolean = false,
-          typeofComponent: string = 'component',
-          usingBeforeEach: boolean = false;
+        const hasStore: boolean = !['router', 'controller', 'store'].includes(typeofComponent) ?  await Confirm.prompt(`insert a store module ?`) : false;
+        const hasProto: boolean = ['router', 'controller', 'store'].includes(typeofComponent) ? true : await Confirm.prompt(`insert a proto element ?`);
         if (hasProto) {
-          typeofComponent = await InputCLI.prompt(`type of the component (component, async, store, router, controller) ?`);
-          usingDef = await Confirm.prompt(`insert definitions (def:) ?`);
-          usingDefault = await Confirm.prompt(`insert default statement (default:) ?`);
-          usingDeclare = await Confirm.prompt(`insert declarations (declare:) ?`);
-          usingBeforeEach = await Confirm.prompt(`insert before-each (before-each:) ?`);
+          usingDef = ['router'].includes(typeofComponent) ? true : await Confirm.prompt(`insert definitions (def:) ?`);
+
+          usingDeclare = !['router'].includes(typeofComponent) ? await Confirm.prompt(`insert declarations (declare:) ?`) : false;
+
+          usingBeforeEach = !['router'].includes(typeofComponent) ? await Confirm.prompt(`insert before-each (before-each:) ?`) : false;
+
+          usingDefault = !['router', 'controller', 'store'].includes(typeofComponent) ? (['async',].includes(typeofComponent) || await Confirm.prompt(`insert default statement (default:) ?`) ) : false;
         }
-        const hasStyle: boolean = await Confirm.prompt(`insert a style element ?`);
-        let cssLang: boolean = false;
+
+        const hasStyle: boolean = !['router', 'controller', 'store'].includes(typeofComponent) ?  await Confirm.prompt(`insert a style element ?`) : false;
+        let cssLang: any | null;
         if (hasStyle) {
-          /*
           cssLang = await Select.prompt({
-            messsage: 'style language: please choose a CSS pre-processor ?',
+            message: 'now choose a CSS pre-processor ?',
             options: [
+              { name: 'Css', value: null },
               { name: 'Sass', value: 'sass' },
               { name: 'Denolus', value: 'denolus' },
             ]
           });
-          */
         }
-        console.warn(pathToFolder, hasProto, hasStyle);
+        let result = this.template(this.templateSTR, {
+          componentName,
+          path: pathToFolder,
+
+          templateImports: this.templateImports,
+          templateProto: hasProto ? this.templateProto : '',
+          templateStyle: hasStyle ? this.templateStyle : '',
+          templateStore: hasStore ? this.templateStore : '',
+
+          styleLang: hasStyle && cssLang ? ` lang="${cssLang}"` : '',
+          componentType: ` type="${typeofComponent}"`,
+          storeNamespace: 'namespace="test"',
+          defaultSTR: usingDefault ? `default: // Typescript: onmount, mounted, didMount...\n    {{ onmount }}` : '',
+          onmount: ['async'].includes(typeofComponent) ? `Async.resolve(this.message);` : '',
+          def: usingDef ? `def: # YAML` : '',
+          declarations: usingDeclare ? `declare: // Typscript\n    public readonly message: string = 'Hello World';` : '',
+          beforeEach: usingBeforeEach ? `before-each: // Typescript` : '',
+          elements: ['async', 'component'].includes(typeofComponent) && usingDeclare? `\n<!--\n<p>\${message}</p>\n-->` : '',
+        });
+        Deno.writeTextFileSync(fullPath, result);
+        console.warn(result);
         break;
     }
   }
