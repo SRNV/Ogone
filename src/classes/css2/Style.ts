8cc4cfa05a05bc6194e1145551bf5d6b5f0778e5
SRNV <alula.rudy@gmail.com>
2021-02-04 16:35:00 +0100

    wip: start Deno integration into components

--- src/classes/css2/Style.ts
+++ src/classes/css2/Style.ts
@@ -6,15 +6,29 @@ import notParsedElements from "../../../utils/not-parsed.ts";
 import read from "../../../utils/agnostic-transformer.ts";
 import { Utils } from "../Utils.ts";
 import type {
+  Expressions,
   ProtocolScriptRegExpList,
+  TypedExpressions,
 } from "../../ogone.main.d.ts";
 import RegExpTransformable from "../RegExpTransformable.ts";
 
 interface StyleOptions {
   vars: { [k: string]: string | StyleOptions["vars"] }
 }
+interface StyleDocumentOptions {
+  text: string;
+  expressions: Expressions;
+  typedExpressions: TypedExpressions
+}
+class StyleDocument implements StyleDocumentOptions {
+  constructor(
+    public text: StyleDocumentOptions['text'],
+    public expressions: StyleDocumentOptions['expressions'],
+    public typedExpressions: StyleDocumentOptions['typedExpressions']
+  ) {}
+}
 export default class Style extends Utils {
-  private static currentDocument: any;
+  private static currentDocument: StyleDocument;
   private static readonly variables: ProtocolScriptRegExpList = [
     // exportable variables
     new RegExpTransformable(/(\b@export)(\s+const(?<evaluated>\*){0,1}\b)(?<name>.+?)(\=)/, () => '')
@@ -25,7 +39,7 @@ export default class Style extends Utils {
   ];
   private static readonly structure: ProtocolScriptRegExpList = [
     // describing all children rules
-    new RegExpTransformable(/(?<=\d+_block|^|\;)(?<selector>[^;]+?)(?<rules>\d+_block)/, () => '')
+    new RegExpTransformable(/(?<=\d+_block|^|\;|\{)(?<selector>[^;]+?)(?<rules>\d+_block)/, () => '')
       .setName('rule'),
     // any property in the rule
     new RegExpTransformable(/(\b.+?)(\s*)(\:)/, () => '')
@@ -56,7 +70,7 @@ export default class Style extends Utils {
    * @param css {string}
    * @param opts {StyleOptions}
    */
-  public transform(css: string, opts: StyleOptions): string {
+  public static transform(css: string, opts: StyleOptions): string {
     // to create the top level of the document
     // we will add some curly and edit the inputs
     // of the end user
@@ -72,6 +86,44 @@ export default class Style extends Utils {
       array: notParsedElements
         .concat(elements),
     });
+    this.setCurrentDocument({
+      expressions,
+      typedExpressions,
+      text: result,
+    });
+    this.parseDocument();
     return result;
   }
+  public static setCurrentDocument(document: StyleDocumentOptions) {
+    this.currentDocument = new StyleDocument(document.text, document.expressions, document.typedExpressions);
+  }
+  /**
+   * main method to start parsing the document
+   * we will use all blocks expressions
+   */
+  public static parseDocument() {
+    if (!this.currentDocument.typedExpressions) return;
+    Object.entries(this.currentDocument.typedExpressions.blocks)
+      .reverse()
+      .forEach(([key, value]) => {
+        console.log(key, value);
+      });
+  }
 }
+const test1 = Style.transform(`
+  @const var1 = test;
+  div {
+    ...$var1;
+    color: red;
+    span {
+      element {
+        a {
+          color: blue;
+        }
+      }
+    }
+  }
+`, {
+  vars: {},
+});
+console.warn(test1);
